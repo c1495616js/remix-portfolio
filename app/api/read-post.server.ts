c58a3dfc198586762aa8633815987db2f301f98a
Jerry Wang <jerryappleid761208@gmail.com>
2022-07-16 00:03:26 -0700

    change blog path;

--- app/api/read-post.server.ts
+++ app/api/read-post.server.ts
@@ -7,8 +7,8 @@ import { bundleMDX } from 'mdx-bundler';
 import type { Frontmatter } from '~/types';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-`${__dirname}, '..', 'app/data/blog'`;
-const MDX_PATH = path.join(__dirname, '..', 'app/data/blog');
+`${process.cwd()}, 'app/data/blog'`;
+const MDX_PATH = path.join(process.cwd(), 'app/data/blog');
 export async function getAllFrontMatters(): Promise<Frontmatter[]> {
   // const pathss = await globby([`${MDX_PATH}/**/*.mdx`]);
 
