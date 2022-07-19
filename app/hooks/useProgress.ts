115258701d5ea925705859c39611db442d397fde
Jerry Wang <jerryappleid761208@gmail.com>
2022-07-19 12:45:59 -0700

    fix loader

--- app/hooks/useProgress.ts
+++ app/hooks/useProgress.ts
@@ -6,15 +6,23 @@ function useProgress() {
   let globalState = useGlobalPendingState();
   const toastRef = React.useRef<string | null>(null);
 
-  if (globalState === 'idle') {
+  function cleanToast() {
     if (toastRef.current) {
       toast.dismiss(toastRef.current);
+      toastRef.current = null;
     }
-    return null;
   }
 
-  const toastId = toast.loading('Loading...');
-  toastRef.current = toastId;
+  React.useEffect(() => {
+    if (globalState === 'idle') {
+      cleanToast();
+    } else {
+      if (!toastRef.current) {
+        const toastId = toast.loading('Loading...');
+        toastRef.current = toastId;
+      }
+    }
+  }, [globalState]);
 }
 
 export default useProgress;
